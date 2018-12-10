53f4cd301d24615c77220995019fd4883e0bed0f
Nick Pfundstein <nickpfundstein@gmail.com>
2018-12-09 21:26:21 -0500

    Unfinished day 9 part 2

--- src/day9/mod.rs
+++ src/day9/mod.rs
@@ -52,3 +52,54 @@ pub fn part1() {
 
     println!("{}", output.len());
 }
+
+pub fn part2() {
+    let mut current_dir = env::current_dir().unwrap();
+    current_dir.push("src/day9/input.txt");
+    let mut file = File::open(current_dir).unwrap();
+    let mut contents = String::new();
+
+    let _bytes = file.read_to_string(&mut contents);
+
+    let mut output = String::new();
+    let mut bytes = contents.trim().bytes();
+    while let Some(byte) = bytes.next() {
+        match byte {
+            b'(' => {
+                let mut buffer_string = String::new();
+                let mut next = bytes.next().unwrap();
+
+                while next != b'x' {
+                    buffer_string.push(next as char);
+                    next = bytes.next().unwrap();
+                }
+
+                let buffer_size = buffer_string.parse::<usize>().unwrap();
+
+                let mut repetition_string = String::new();
+                next = bytes.next().unwrap();
+
+                while next != b')' {
+                    repetition_string.push(next as char);
+                    next = bytes.next().unwrap();
+                }
+
+                let total_repetition = repetition_string.parse::<usize>().unwrap();
+
+                let mut buffer = String::new();
+                for _i in 0..buffer_size {
+                    buffer.push(bytes.next().unwrap() as char);
+                }
+
+                for _i in 0..total_repetition {
+                    output.push_str(&buffer);
+                }
+            },
+            _ => {
+                output.push(byte as char);
+            }
+        }
+    }
+
+    println!("{}", output.len());
+}
